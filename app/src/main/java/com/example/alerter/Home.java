b6a8fdc5d05e67ff41bffa746d73f351eee481e2
tharun571 <63015429+tharun571@users.noreply.github.com>
2020-07-25 22:38:09 +0530

    Finding location, address and saving via shared preff

---
+++ app/src/main/java/com/example/alerter/Home.java
@@ -0,0 +1,28 @@
+package com.example.alerter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+
+public class Home extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+
+    }
+
+
+
+
+}
--- app/src/main/java/com/example/alerter/MainActivity.java
+++ app/src/main/java/com/example/alerter/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.alerter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.alerter.Walkthrough.PrefManager;
+import com.example.alerter.Walkthrough.WelcomeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PrefManager prefManager = new PrefManager(getApplicationContext());
+        if (prefManager.isFirstTimeLaunch()) {
+            prefManager.setFirstTimeLaunch(false);
+            startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
+            finish();
+        }
+
+
+        startActivity(new Intent(this,Home.class));
     }
 }
